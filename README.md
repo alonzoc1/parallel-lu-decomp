b0cb6f9aa554efa12d7565b77036c4c535aee698
alonzo <alonzocastanon@live.com>
2024-05-01 23:21:04 -0700

    Add test runner and update README

--- README.md
+++ README.md
@@ -6,6 +6,16 @@ To build gaussian serial implementation, use `g++ gaussian_serial.cpp matrix_rea
 
 To build MPI parallel gaussian implementation, use `mpic++ gaussian_parallel_mpi.cpp matrix_reader.cpp -o gaussian_mpi`. Run it with `mpiexec -np NUM_PROCS ./gaussian_mpi filename.csv MAT_SIZE` or `mpiexec -np NUM_PROCS ./gaussian_mpi filename.mtx MAT_SIZE` where `NUM_PROCS` is the number of processes to provide the algorithm, and MAT_SIZE is the size of the matrix, which needs to be provided beforehand. This is because this parallel implementation has to allocate all the contiguous memory before loading the matrix.
 
+Finally, a required component to run our automated testing code is the matrix generator. Please compile it before hand using `g++ matrix_gen.cpp -o gen`. It is used to generate random matrices.
+
 Eigen and Fast Matrix Market headers are included in this repository. For serial timing, boost library is required (commonly found, search for steps specific to your OS, or see generic steps here: https://www.boost.org/doc/libs/1_55_0/doc/html/bbv2/installation.html)
 
 Fast Matrix Market may require certain dependencies to be installed on your system (https://github.com/alugowski/fast_matrix_market)
+
+# Test cases and how to replicate tests
+## MPI and serial
+Results are stored in raw_serial.txt and raw_mpi.txt at this point. `run_tests.cpp` can be compiled (only uses boost timer) and run with `./run_tests TEST_TYPE AVG_OVER` where TEST_TYPE is `serial` or `mpi`, and AVG_OVER is how many iterations per tested size. Tested matrix sizes are 64, 256, 512, 1024, 2048. For each iteration a random matrix of that size is generated using `matrix_gen.cpp` (expected to be compiled as `gen`), and is run using the compiled serial or mpi implementation.
+
+For example, to test each size 3 times for serial, run `./run_tests serial 3`, which will output to `raw_serial.txt` (appends to file, so delete or rename this file for a clean run).
+
+When MPI is run in this way, automatically tests with process counts 2, 4, and 8.
---
+++ run_tests.cpp
@@ -0,0 +1,70 @@
+
+#include <cstdlib>
+#include <iostream>
+#ifndef BOOST_TIMER_ENABLE_DEPRECATED
+#define BOOST_TIMER_ENABLE_DEPRECATED
+#endif
+#include <boost/timer.hpp>
+
+using namespace std;
+
+int AVG_OVER;
+
+string build_test_data_filename(int iteration, int size) {
+    string test_data_prefix = "./data/test_data_";
+    test_data_prefix += to_string(size) + "_" + to_string(iteration) + ".csv";
+    return test_data_prefix;
+}
+
+/* Generates random matrices for sizes 64, 256, 512, 1024, and 2048, then tests them the given amount of times
+Saves results to results_TYPE.txt, and raw outputs to raw_TYPE.txt, where TYPE is the test type
+*/
+int main(int argc, char* argv[]) {
+    string usage = "Usage: ./run_tests TEST_TYPE AVG_OVER\nWhere TEST_TYPE is serial or mpi, and AVG_OVER is how many iterations to run each matrix";
+    if (argc != 3) {
+        cout << usage << endl;
+        return 1;
+    }
+    string test_type = argv[1];
+    AVG_OVER = stoi(argv[2]);
+    string raw_filename = "raw_" + test_type + ".txt";
+    string results_filename = "results_" + test_type + ".txt";
+    int test_sizes[5] = {64, 256, 512, 1024, 2048};
+    // Generate data first
+    
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < AVG_OVER; j++) {
+            cout << "Generating data for " << test_sizes[i] << " iteration " << j << endl;
+            if (system(("./gen " + build_test_data_filename(j, test_sizes[i]) + " " + to_string(test_sizes[i])).c_str()) != 0) {
+                cout << "Generating data failed, exiting..." << endl;
+                return 1;
+            }
+            sleep(1); // let seed increment
+        }
+    }
+    // Run tests
+    for (int i = 0; i < 5; i++) {
+        int test_size = test_sizes[i];
+        // Start timer
+        cout << "Starting timer for size: " << test_size << endl;
+        boost::timer myTimer;
+        for (int iter_tracker = 0; iter_tracker < AVG_OVER; iter_tracker++) {
+            cout << "--Iteration " << iter_tracker << endl;
+            if (test_type == "serial") {
+                system(("echo SIZE" + to_string(test_size) + "ITER" + to_string(iter_tracker) + " >> " + raw_filename).c_str());
+                if (system(("./gaussian_serial " + build_test_data_filename(iter_tracker, test_size) + ">> " + raw_filename).c_str()) != 0) {
+                    cout << "One of the tests failed, exiting..." << endl;
+                }
+            } else if (test_type == "mpi") {
+                int proc_counts[3] = {2, 4, 8};
+                for (int proc_i = 0; proc_i < 3; proc_i++) {
+                    system(("echo SIZE" + to_string(test_size) + "ITER" + to_string(iter_tracker) + "PROCS" + to_string(proc_counts[proc_i]) + " >> " + raw_filename).c_str());
+                    if (system(("mpiexec -np " + to_string(proc_counts[proc_i]) + " ./gaussian_mpi " + build_test_data_filename(iter_tracker, test_size) + " " + to_string(test_size) + " >> " + raw_filename).c_str()) != 0) {
+                        cout << "One of the tests failed, exiting..." << endl;
+                    }
+                }
+
+            }
+        }
+    }
+}
