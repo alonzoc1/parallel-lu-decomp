36365e767c2c84269dd32d983bd4fe851e9d5cd2
raul <raulbernalcollege2015@gmail.com>
2024-05-01 21:04:57 -0700

    I think I fixed the issue, it works well now with openmp

--- PCludcmp.cpp
+++ PCludcmp.cpp
@@ -9,29 +9,31 @@
 #ifdef _OPENMP
 #include <omp.h>
 #endif
+#ifdef _MPI
+#include <mpi.h>
+#endif
+#define PI 3.1415926
 
 using namespace std;
 using namespace Eigen;
 
-template <typename Derived> void PCludcmp(MatrixBase<Derived> &a, long n) {
+template <typename Derived>
+void PCludcmp(MatrixBase<Derived> &a, long n, int *indx) {
     int i, imax, j, k;
     double big, dum, sum;
     double *scaling;
     double TINY = 1.0e-20;
+    // mpi parameters
+    int comm_sz, my_rank;
+    // MPI_Init()
 
     scaling = new double[n];
 
     // implicit scaling. We find the scaling factor and save it.
     // no actual scaling is made here. Row i is implicitly scaled by scaling[i]
-    // TODO: This part is easily doable with openMP
 #pragma omp parallel for default(none) private(i, big) shared(scaling, a, n)
     for (i = 0; i < n; i++) {
-        // big = 0.0;
         big = a.row(i).cwiseAbs().maxCoeff(); // the max abs(value) of i_th row
-        // for (j = 0; j < n; j++){
-        //     if(abs(a(i,j)) > big)
-        //         big = abs(a(i,j));
-        // }
         if (big == 0.0) {
             printf("Singular matrix in routine ludcmp");
             exit(1);
@@ -41,79 +43,107 @@ template <typename Derived> void PCludcmp(MatrixBase<Derived> &a, long n) {
 
     // main crout's loop. iterate by column
     for (j = 0; j < n; j++) {
-        // build upper trianfular part up to j-1 row
-#pragma omp parallel for default(none) private(i, k, sum) shared(a, j)
-        for (i = 0; i < j; i++) { // this can be parallelize with Openmp
+// build upper trianfular part up to j-1 row since eache thread will have its own set of i's,
+// no overlap the a(k,j) term would have already been build in previous iterations of j
+#pragma omp parallel for default(none) private(i, k, sum) shared(a, j)         \
+    schedule(guided)
+        for (i = 0; i < j; i++) {
             sum = a(i, j);
             for (k = 0; k < i; k++)
-                // since eache thread will have its own set of i's, no overlap
-                // the a(k,j) term would have already been build in previous
-                // iterations of j
-                sum -= a(i, k) * a(k, j); // we are not reducing to sum, sum is
-                                          // a private variable
+               
+                sum -= a(i, k) * a(k, j); // we are not reducing to sum
 
-            // no need for critical section because i's do not overlap
             a(i, j) = sum;
         }
-
-        // find larget pivot element. Also compute i = j up to n rows
-        big = 0.0;
-        // parallelization here is very similar to the previous loop
-        // FIXME: Problem seems to be with his loop. when I un-parallelize it it works
-#pragma omp parallel for default(none) private(i, sum, k, dum)                 \
-        shared(a, scaling, imax, j, n) reduction(max : big)
+    
+#pragma omp parallel for default(none) private(i, sum, k)                      \
+    shared(a, j, n, big, imax, cout) schedule(static)
         for (i = j; i < n; i++) {
             sum = a(i, j);
             for (k = 0; k < j; k++)
                 sum -= a(i, k) * a(k, j);
 
             a(i, j) = sum;
-            // check if current pivot is the biggest. Only one thread at a time
-            // prevents read-write racing condition
-#pragma omp critical
-            {
-                if ((dum = scaling[i] * abs(sum)) >= big) {
-                    big = dum; // return only the max value of big
-                    imax = i;  // we are keeping track of the index of the pivot
-                }
+            // if(j == n-1) // print a(n,n)
+            cout << "a_ij " << i << "," << j << " " << a(i, j) << endl;
+        }
+
+        // look for pivot. This area is not worth parallelizing since we will have to make the
+        // conditional a critical section. We interested in the row-index of max(big), not it's value.
+        // the operatio is scalar-scalar multiplication
+        big = 0.0;
+        for (i = j; i < n; i++) {
+            dum = scaling[i] * abs(a(i, j));
+            if (dum >= big) {
+                big = dum;
+                imax = i;
             }
         }
 
-        // swap pivot rows.
+        // swap pivot rows. No parallelization
         if (j != imax) {
             a.row(imax).swap(a.row(j));
-            // for (k = 0; k < n; k++){
-            // 	dum = a(imax,k);
-            // 	a(imax,k) = a(j,k);
-            // 	a(j,k) = dum;
-            // }
             scaling[imax] = scaling[j];
         }
 
+        indx[j] = imax; // needed for backsubtitution if solveing Ax = b
         if (a(j, j) == 0.0)
             a(j, j) = TINY;
         if (j != n - 1) {
             dum = (1.0 / a(j, j));
-# pragma omp parallel for default(none) private(i) shared(j, n, a, dum)
+#pragma omp parallel for default(none) private(i, k) shared(j, n, a, dum)      \
+    schedule(static)
             for (i = j + 1; i < n; i++)
                 a(i, j) *= dum;
         }
     }
 }
 
-int main() {
-    long n = 3;
+template <typename Derived>
+void backsub(MatrixBase<Derived> &a, int n, int *indx, double b[]) {
+    int i, ii = 0, ip, j;
+    double sum;
+    for (i = 0; i < n; i++) {
+        ip = indx[i];
+        sum = b[ip];
+        b[ip] = b[i];
+        if (ii != 0)
+            for (j = ii - 1; j < i; j++)
+                sum -= a(i, j) * b[j];
+        else if (sum != 0.0)
+            ii = i + 1;
+        b[i] = sum;
+    }
+    for (i = n - 1; i >= 0; i--) {
+        sum = b[i];
+        for (j = i + 1; j < n; j++)
+            sum -= a(i, j) * b[j];
+        b[i] = sum / a(i, i);
+    }
+}
+
+int main(int argc, char **argv) {
+    long n = (long)stoi(argv[1]);
     MatrixXd a(n, n); // test matrix
-
+    // initParallel();
+    int *indx = new int[n];
+    double b[] = {1, 1, -2, -2};
     // init matrix
-    a << 1.0, 2.0, -1.0, 6.0, -5.0, 4.0, -9.0, 8.0, -7.0;
-    cout << a << endl;
-
+    // a << 1.0, 2.0, -1.0, 6.0, -5.0, 4.0, -9.0, 8.0, -7.0;
+    a << 1, 0, 0, 1, 3, 2, 1, 0, 0, 0, 0, -1, 0, -1, 0, -2;
+    // a = MatrixXd::Random(n, n);
+    // a = a + (MatrixXd::Constant(n, n, 1) *
+    //          100.0); // random n x n matrix doubles between -100, 100
+    //  cout << a << endl;
+    //  cout << a.lu().matrixLU() << endl; // print eigen's lu decomp partial
+    //  pivot
+    printf("\n");
     // in place LU decomp from Eigen for testing.
     // PartialPivLU<Ref<RowMajorMatrixXd>> lu(a);
-
-    PCludcmp(a, n);
-    cout << a << endl;
+    // cout << a.col(1).maxCoeff(&index) << " index" << index << endl;
+    PCludcmp(a, n, indx);
+    std::cout << a << endl; // print our version
+    backsub(a, n, indx, b);
 
     return 0;
 }
--- SCludcmp.cpp
+++ SCludcmp.cpp
@@ -1,97 +1,123 @@
 // Only needed to eliminate intelliSennse givng false errors about Eigen.
-// Not needed to compile, just gets rid of the false error squiggles 
+// Not needed to compile, just gets rid of the false error squiggles
 #if __INTELLISENSE__
 #undef __ARM_NEON
 #undef __ARM_NEON__
 #endif
 
+#include "Eigen/Dense"
 #include <cmath>
 #include <iostream>
-#include "Eigen/Dense"
+#define PI 3.1415926
 
 using namespace std;
 using namespace Eigen;
 
 // Accepts any type of dense matrix. No sparse
 template <typename Derived>
-void SCludcmp(MatrixBase<Derived>& a, long n) {
+void SCludcmp(MatrixBase<Derived> &a, long n, int *indx) {
     int i, imax, j, k;
     double big, dum, sum, temp;
     double *scaling;
     double TINY = 1.0e-20;
 
-	scaling = new double[n];
-	// *d = 1.0;
-	for (i = 0; i < n; i++) {
-		big = a.row(i).cwiseAbs().maxCoeff(); // the max abs(value) of i_th row
-		// big = 0.0;
-		// for (j = 0; j < n; j++){
-        //     temp = abs(a(i,j));
-        //     if(temp > big) big = temp;
-        // }
-		if (big == 0.0){ 
+    scaling = new double[n];
+    // *d = 1.0;
+    for (i = 0; i < n; i++) {
+        big = a.row(i).cwiseAbs().maxCoeff(); // the max abs(value) of i_th row
+
+        if (big == 0.0) {
             printf("Singular matrix in routine ludcmp");
             exit(1);
         }
-	    scaling[i] = 1.0 / big;
+        scaling[i] = 1.0 / big;
     }
-	
-	for (j = 0; j < n; j++) {
-		for (i = 0; i < j; i++) {
-			sum = a(i,j);
-			for (k = 0; k < i; k++) 
-                sum -= a(i,k) * a(k,j);
-			
-            a(i,j) = sum;
-		}
-		big = 0.0;
-		for (i = j; i < n; i++) {
-			sum = a(i,j);
-			for (k = 0; k < j; k++) 
-                sum -= a(i,k) * a(k,j);
-			
-            a(i,j) = sum;
-			if ((dum = scaling[i] * abs(sum)) >= big) {
-				big = dum;
-				imax = i;
-			}
-		}
-		if (j != imax) {
+
+    for (j = 0; j < n; j++) {
+        for (i = 0; i < j; i++) {
+            sum = a(i, j);
+            for (k = 0; k < i; k++)
+                sum -= a(i, k) * a(k, j);
+
+            a(i, j) = sum;
+        }
+        big = 0.0;
+        for (i = j; i < n; i++) {
+            sum = a(i, j);
+            for (k = 0; k < j; k++)
+                sum -= a(i, k) * a(k, j);
+
+            a(i, j) = sum;
+            // if(j == n-1) // print a(n,n)
+            cout << "a_ij " << i << "," << j << " " << a(i, j) << endl;
+
+            if ((dum = scaling[i] * abs(sum)) >= big) {
+                big = dum;
+                imax = i;
+            }
+        }
+        // cout << "imax " << imax << endl;
+        if (j != imax) {
             a.row(imax).swap(a.row(j));
-			// for (k = 0; k < n; k++){
-			// 	dum = a(imax,k);
-			// 	a(imax,k) = a(j,k);
-			// 	a(j,k) = dum;
-			// }
-			scaling[imax] = scaling[j];
-		}
-        if(a(j,j) == 0.0) a(j,j) = TINY;
-		if (j != n-1) {
-			dum = (1.0 / a(j,j));
-			for (i = j + 1; i < n; i++) a(i,j) *= dum;
-		}
-	}
+            scaling[imax] = scaling[j];
+        }
+        indx[j] = imax;
+        if (a(j, j) == 0.0)
+            a(j, j) = TINY;
+        if (j != n - 1) {
+            dum = (1.0 / a(j, j));
+            for (i = j + 1; i < n; i++)
+                a(i, j) *= dum;
+        }
+    }
 }
 
-int main(){
-	long n = 3;
-	MatrixXd a(n,n); // test matrix
-	
+template <typename Derived>
+void backsub(MatrixBase<Derived> &a, int n, int *indx, double b[]) {
+    int i, ii = 0, ip, j;
+    double sum;
+    for (i = 0; i < n; i++) {
+        ip = indx[i];
+        sum = b[ip];
+        b[ip] = b[i];
+        if (ii != 0)
+            for (j = ii - 1; j < i; j++)
+                sum -= a(i, j) * b[j];
+        else if (sum != 0.0)
+            ii = i + 1;
+        b[i] = sum;
+    }
+    for (i = n - 1; i >= 0; i--) {
+        sum = b[i];
+        for (j = i + 1; j < n; j++)
+            sum -= a(i, j) * b[j];
+        b[i] = sum / a(i, i);
+    }
+}
 
-	// init matrix
-    a << 1.0, 2.0, -1.0,
-        6.0, -5.0, 4.0,
-        -9.0, 8.0, -7.0;
-        cout << a <<endl;
+int main(int argc, char **argv) {
+    long n = (long)stoi(argv[1]);
+    MatrixXd a(n, n); // test matrix
+    // initParallel();
+    int *indx = new int[n];
+    //double b[] = {2 * PI, 5 * PI, -8 * PI};
+	double b[] = {1, 1, -2, -2};
+    // init matrix
+   // a << 1.0, 2.0, -1.0, 6.0, -5.0, 4.0, -9.0, 8.0, -7.0;
+   a << 1, 0, 0, 1, 3, 2, 1, 0, 0, 0, 0, -1, 0, -1, 0, -2;
+    // a = MatrixXd::Random(n, n);
+    // a = a + (MatrixXd::Constant(n, n, 1) *
+    //          100.0); // random n x n matrix doubles between -100, 100
+    //  cout << a << endl;
+    //  cout << a.lu().matrixLU() << endl; // print eigen's lu decomp partial
+    //  pivot
+    printf("\n");
+    // in place LU decomp from Eigen for testing.
+    // PartialPivLU<Ref<RowMajorMatrixXd>> lu(a);
+    // cout << a.col(1).maxCoeff(&index) << " index" << index << endl;
+    SCludcmp(a, n, indx);
+    cout << a << endl; // print our version
+    backsub(a, n, indx, b);
 
-		// in place LU decomp from Eigen for testing.
-		//PartialPivLU<Ref<RowMajorMatrixXd>> lu(a);
-
-
-        SCludcmp(a, n);
-        cout << a << endl;
-
-
-    
     return 0;
 }
